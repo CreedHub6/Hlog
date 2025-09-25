d36c83b5a23b5668b48c9dfff520d80ad32e080b
CreedHub6 <liljezzro@gmail.com>
2025-09-25 18:11:03 +0100

    Hlog Update

--- log_analysis/settings.py
+++ log_analysis/settings.py
@@ -64,8 +64,8 @@ WSGI_APPLICATION = 'log_analysis.wsgi.application'
 
 # Database configuration
 
-DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', } }
-}
+DATABASES = { 'default': { 'ENGINE': 'django.db.backends.sqlite3', 'NAME': BASE_DIR / 'db.sqlite3', }
+             }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
